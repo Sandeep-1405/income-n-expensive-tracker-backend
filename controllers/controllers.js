87f59b3861d97ef84f5e7aed494c40f0812fd5fa
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-09-26 21:36:01 +0530

    search functionality

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,7 +10,6 @@ const db = admin.firestore();
 const getWorkersDetails = async(req,res)=>{
     try {
         const { owner } = req.params;
-        console.log(owner)
         const workerList = [];
     
         const workersSnapshot = await db.collection('workers').where('owner', '==', owner).get();
@@ -41,8 +40,61 @@ const createworker = async(req,res) =>{
     res.status(201)
 }
 
+const searchByName = async(req,res)=>{
+  const {inputText} = req.params;
+  try{
+    const listByNames = []
+    const listByNamesSnapshot = await db.collection('workers').where('name',"==",inputText).get();
+
+    if (listByNamesSnapshot.empty) {
+      return res.status(404).json({ success: false, message: 'No workers found for the given Name.' });
+    }
+
+    listByNamesSnapshot.forEach(doc => {
+      listByNames.push({
+        id: doc.id,
+        ...doc.data()
+      });
+    });
+    console.log(listByNames)
+    res.status(200).json({ success: true, workers: listByNames });
+
+  } catch (error) {
+    console.error('Error fetching worker details:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch worker details', error });
+  }
+
+}
+
+const searchByArea = async(req,res)=>{
+  const {inputText} = req.params;
+  try{
+    const listByArea = []
+    const listByAreasSnapshot = await db.collection('workers').where('area',"==",inputText).get();
+
+    if (listByAreasSnapshot.empty) {
+      return res.status(404).json({ success: false, message: 'No workers found for the given area.' });
+    }
+
+    listByAreasSnapshot.forEach(doc => {
+      listByArea.push({
+        id: doc.id,
+        ...doc.data()
+      });
+    });
+    console.log(listByArea)
+    res.status(200).json({ success: true, workers: listByArea });
+
+  } catch (error) {
+    console.error('Error fetching worker details:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch worker details', error });
+  }
+
+}
 
 module.exports = {
     createworker,
-    getWorkersDetails
+    getWorkersDetails,
+    searchByName,
+    searchByArea
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const {createworker, getWorkersDetails} = require('../controllers/controllers')
+const {createworker, getWorkersDetails, searchByName,searchByArea} = require('../controllers/controllers')
 
 const router = express.Router();
 
 router.get('/workers/:owner',getWorkersDetails)
 
 router.post('/workers', createworker)
+
+router.get('/search-by-name/:inputText', searchByName) //query according to the owner
+
+router.get('/search-by-area/:inputText', searchByArea) //query according to the owner
 
 module.exports = router
