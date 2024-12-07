167cbcaea4f380424145f43dbd7cde79a0bf55dc
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-12-07 23:22:18 +0530

    new domin added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const router = require('./routes/routes');
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["https://income-n-expensive-tracker.vercel.app","http://localhost:3000"]
+    origin: ["https://income-n-expensive-tracker.vercel.app","http://localhost:3000","https://workers-management-gilt.vercel.app/"]
 }));
   
 app.use(router)
