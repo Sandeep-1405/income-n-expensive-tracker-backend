2fd11ddb3878f4c69c5246e5e3b013471802c693
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-12-07 23:01:51 +0530

    frontend url added in cors

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const router = require('./routes/routes');
 
 const app = express()
 app.use(express.json())
-app.use(cors())
-
+app.use(cors({
+    origin: "https://income-n-expensive-tracker.vercel.app"
+}));
+  
 app.use(router)
 
 app.listen(process.env.port , ()=>{
